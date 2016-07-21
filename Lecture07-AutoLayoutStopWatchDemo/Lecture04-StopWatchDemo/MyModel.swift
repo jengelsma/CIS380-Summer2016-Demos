bbba1c8028cc56f46f4a8bdd6ce71b6a0856cbe6
Jonathan Engelsma <jengelsma@gmail.com>
2016-07-21 11:20:07 -0400

    Added Lecture 07 autolayout demo

---
+++ Lecture07-AutoLayoutStopWatchDemo/Lecture04-StopWatchDemo/MyModel.swift
@@ -0,0 +1,26 @@
+//
+//  MyModel.swift
+//  Lecture04-StopWatchDemo
+//
+//  Created by Jonathan Engelsma on 7/19/16.
+//  Copyright © 2016 Jonathan Engelsma. All rights reserved.
+//
+
+import Foundation
+
+struct MyModel {
+    var startTime: NSDate
+    var pauseTime: NSDate
+    var nightMode: Bool = false
+    var timing: Bool = false
+    var stopWatchTimer: NSTimer
+    
+    
+    init() {
+        startTime = NSDate()
+        pauseTime = NSDate()
+        stopWatchTimer = NSTimer()
+    }
+    
+    
+}
