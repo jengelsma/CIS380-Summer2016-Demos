606a4002b00dc37476d26e7e103d5142511188fc
Jonathan Engelsma <jengelsma@gmail.com>
2016-07-26 15:12:33 -0400

    Added Lecture 11 demo

---
+++ Lecture11-FireBaseDemo/Pods/Target Support Files/Pods-Lecture11-FireBaseDemo/Pods-Lecture11-FireBaseDemo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Lecture11_FireBaseDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Lecture11_FireBaseDemoVersionString[];
+
---
+++ Lecture11-FireBaseDemo/Pods/Target Support Files/Pods-Lecture11-FireBaseDemoUITests/Pods-Lecture11-FireBaseDemoUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Lecture11_FireBaseDemoUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Lecture11_FireBaseDemoUITestsVersionString[];
+
